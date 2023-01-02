adc6a1bbfa81bd9d5c67b975a921ef8cb8183a24
Krutika <skyparadise37@gmail.com>
2023-01-03 01:29:40 +0530

    hero, demo, container updated in index.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,25 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "header-bg": "url('../images/site_showcase_bg.jpg')",
+        "hero-img": "url('../images/site_showcase_bg.jpg')",
       },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        'md':'800px',
+        'lg': '800px',
+        'xl': '800px',
+        '2xl': '1000px',
+      }
+    },
+    screens: {
+      'md':'911px',
     },
   },
   daisyui: {
     themes: ["light", "dark"],
   },
-  plugins: [require("daisyui"), require("ejs")],
+  plugins: [require("daisyui")],
 };
